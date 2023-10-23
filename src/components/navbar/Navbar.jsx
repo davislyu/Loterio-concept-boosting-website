c91c630ed3334fd6c3ccd82e38c442ccbd19afc4
davislyu <davislyu@gmail.com>
2023-10-23 16:56:52 +0300

    Completed Navbar component and Home page

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,51 @@
+import './Navbar.css'
+import {useState} from "react"
+import { Link } from 'react-router-dom';
+import {GiOpenTreasureChest} from "react-icons/gi"
+import {TfiClose} from "react-icons/tfi"
+import {MdOutlineMenuOpen} from "react-icons/md"
+import wowLogo from "../../assets/world-of-warcraft.png"
+
+const Navbar = () => {
+    const [active,setActive] = useState("noToggle")
+    const handleToggle = () => {
+        if (active === "toggle") {
+            setActive("noToggle");
+        } else {
+            setActive("toggle");
+        }
+    }
+    
+
+    return(
+        <nav className="nav">
+            <div className="logo"><img src={wowLogo} alt="" /><span>Loterio.</span></div>
+            <ul className={`navlinks ${active}`}>
+                <li className="navlink">
+                <Link to="/">Home</Link>
+                </li>
+                <li className="navlink">
+                <Link to="/about">About</Link>
+                </li>
+                <li className="navlink">
+                <Link to="/order">Order</Link>
+                </li>
+                <li className="navlink">
+                <Link to="/contact">Contact</Link>
+                </li>
+                <li className="navlink">
+                <Link to="/info">Info</Link>
+                </li>
+            </ul>
+            <div onClick={handleToggle} className="icon">
+                {active==="noToggle" ? <MdOutlineMenuOpen/> : <TfiClose/>  }
+            </div>
+
+
+
+        </nav>
+    )
+
+}
+
+export default  Navbar
